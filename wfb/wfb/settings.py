fbd43047836ec343fd4f7b49d27cfeaba745155b
Marcin <sommer.marcin@gmail.com>
2020-12-14 21:22:35 +0100

    heroku first deploy

--- wfb/wfb/settings.py
+++ wfb/wfb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -126,3 +127,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
